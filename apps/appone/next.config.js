2a33c1cb366144bf5deddeff9e3a09d9f05d2007
Jitendra Berde <berde.jitendra14@gmail.com>
2024-09-07 10:07:44 +0530

    Initial changes

---
+++ apps/appone/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(
+  {
+    reactStrictMode: true, // Enable React strict mode for improved error handling
+    swcMinify: true,      // Enable SWC minification for improved performance
+    compiler: {
+      removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    }
+  }
+);
